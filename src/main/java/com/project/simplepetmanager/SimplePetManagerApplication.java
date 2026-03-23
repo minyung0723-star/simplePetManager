424a01e7dbc8e6577f07652155d29f35fe51126d
minyung0723@gmail.com <minyung0723@gmail.com>
2026-03-23 14:44:58 +0900

    Initial commit

---
+++ src/main/java/com/project/simplepetmanager/SimplePetManagerApplication.java
@@ -0,0 +1,13 @@
+package com.project.simplepetmanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimplePetManagerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SimplePetManagerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/project/simplepetmanager/SimplePetManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.simplepetmanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimplePetManagerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
