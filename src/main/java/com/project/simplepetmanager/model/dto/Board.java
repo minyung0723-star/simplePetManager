271a9c7fcfb1a553414cea87a1463549b4d3b924
seunghyunh223 <145409608+seunghyunh223@users.noreply.github.com>
2026-03-27 18:05:37 +0900

    boardDetail api key 최신화, board파일 완성

--- src/main/java/com/project/simplepetmanager/model/dto/Board.java
+++ src/main/java/com/project/simplepetmanager/model/dto/Board.java
@@ -1,4 +1,26 @@
 package com.project.simplepetmanager.model.dto;
 
+import lombok.*;
+
+@Getter
+@Setter
+@ToString
+@AllArgsConstructor
+@NoArgsConstructor
 public class Board {
+    private int store_id;
+    private String store_address;
+    private int store_name;
+    private String store_image;
+    private int store_phone;
+    private int category;
+    private double latitude;
+    private double longitude;
+
+    // ======= bookmark Variables ==========
+    private int user_number;
+    private int storeId;
+    private boolean prefer;
+
+
 }
