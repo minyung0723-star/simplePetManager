0aa80d1da73a9011fe70102afde436000117e8c5
TJ <tjoeun.jr5@gmail.com>
2026-04-07 12:57:10 +0900

    todo ReviewApiController / String insertReview(Review review) 기능 수정하기

---
+++ src/main/java/com/project/simplepetmanager/controller/api/TODOTODO.java
@@ -0,0 +1,29 @@
+package com.project.simplepetmanager.controller.api;
+
+public class TODOTODO {
+// TODO:
+    /**
+     * Service로 가져가서 Contoller 로 전달하게 하기
+     *         // 1. 병원 ID (null이거나 0이면 1번으로 고정)
+     *         if (review.getStoreId() == null || review.getStoreId() == 0) {
+     *             review.setStoreId(1);
+     *         }
+     *
+     *         // 2. 유저 번호 (null이면 1번으로 고정)
+     *         if (review.getUserNumber() == null || review.getUserNumber() == 0) {
+     *             review.setUserNumber(1);
+     *         }
+     *
+     *         // 3. 별점 (이번 에러의 원인! null이면 임시로 5점 부여)
+     *         if (review.getRating() == null) {
+     *             review.setRating(5.0);
+     *         }
+     *
+     *         // 4. 카테고리 (null 방지)
+     *         if (review.getCategory() == null) {
+     *             review.setCategory("병원");
+     *         }
+     *
+     *
+     */
+}
