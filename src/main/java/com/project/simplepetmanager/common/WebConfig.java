401260bb476d0dcd750f4a107fe1d32519fb6804
minyung0723@gmail.com <minyung0723@gmail.com>
2026-04-15 11:56:43 +0900

    enableAscync추가

--- src/main/java/com/project/simplepetmanager/common/WebConfig.java
+++ src/main/java/com/project/simplepetmanager/common/WebConfig.java
@@ -2,11 +2,13 @@ package com.project.simplepetmanager.common;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 
 @Configuration
+@EnableAsync
 @RequiredArgsConstructor
 public class WebConfig implements WebMvcConfigurer{
 
